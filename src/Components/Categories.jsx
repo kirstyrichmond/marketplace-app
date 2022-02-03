70ecfda5073044d31b788fac070905a4d7ec642c
James_G <jamesg.chef@gmail.com>
2022-02-03 20:12:43 +0000

    Add functionality to category drop down

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -4,21 +4,39 @@ import { getCategories } from "../Utils/api";
 const Categories = () => {
   const [categories, setCategories] = useState([]);
 
+  const [catSelection, setCatSelection] = useState("default");
+
   useEffect(() => {
     getCategories().then((res) => {
       setCategories(res);
     });
   }, []);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCatSelection("default");
+  }
+
+  function handleChange(event) {
+    setCatSelection(event.target.value);
+  }
+
   return (
     <>
       <div>
-        <h1>In categories</h1>
-        <select>
-          {categories.map((category) => {
+        <form onSubmit={handleSubmit}>
+          <select
+            className="select"
+            name="category"
+            onChange={handleChange}
+            value={catSelection}
+          >
+            <option value="default">All</option>
+            {categories.map((category) => {
               return <option>{category.category_name}</option>;
-          })}
-        </select>
+            })}
+          </select>
+        </form>
       </div>
     </>
   );
